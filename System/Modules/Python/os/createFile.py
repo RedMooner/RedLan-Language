db421b16ad800925942a46bb3b4d68e061de250c
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2022-08-08 13:25:19 +0500

    New update: now you can pass variable
    now you can pass variables to function parameters
    Example:
    use the OS;
    $value = Hello, World;
    $name = file.txt;
    CreateFile($name,$value);
    operating room
    $a = 1;
    $b = 1;
    python(test.py > $a,$b);

--- System/Modules/Python/os/createFile.py
+++ System/Modules/Python/os/createFile.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 class Module:
    def main(self,args):  
-      my_file = open("some.txt", "w")
+      my_file = open(str(args[0]), "w")
       my_file.write(str(args[1]))
       my_file.close()
       return "file created " + args[0] + " " +  args[1]
